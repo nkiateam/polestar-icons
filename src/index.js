4ded645f37057032b06954cf3ec958c82bfe4722
Hyun Seung Jeong <ahribori@gmail.com>
2018-01-24 15:54:57 +0900

    Icons Component 작성, Generate 된 폰트 업로드

--- src/index.js
+++ src/index.js
@@ -1,42 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import './css/polestar.css';
+import './css/sprite.css';
 
-/*
-1. font-awesome 하고 자체제작 icon font 하고는 폰트파일이 같은가? 따로 관리하는가?
-2. prefix 로 fa 안쓰고 ps 등 다른 것을 사용 할 수 있는가?
-3. 아이콘 클래스 명 규칙 정하기(icon font/sprite)
-예) <i class="fa fa-bar-chart fa-2x">
-4. icon font 와 sprite 간에 서로 중복되는 아이콘 허용?
-5. 아이콘 사이즈 정의해야 한다.
-*/
-
-const Icon = ({ type, name, size }) => {
-    const prefix = type === 'font' ? 'fa' : 'ps-img';
-    const classString = classNames(prefix, {
-        [`${prefix}-${name}`]: true,
-        [`fa-${size}`]: type === 'font' && size !== '',
-    });
-    return (
-        <i className={classString} />
-    );
-};
-
-Icon.propTypes = {
-    /**
-     * font icon 인지 image icon 인지 설정
-     * @default font
-     */
-    type: PropTypes.oneOf(['font', 'image']).isRequired,
-    /** The name of Icon */
+const propTypes = {
+    /** 아이콘 이름 */
     name: PropTypes.string.isRequired,
-    /** The size of Icon */
-    size: PropTypes.oneOf(['', 'sm', 'md', 'lg']).isRequired,
+    /** 폰트 아이콘 / 이미지 스프라이트 아이콘 */
+    type: PropTypes.oneOf(['font', 'image']),
+    /** 아이콘 사이즈(px) 폰트 아이콘에만 적용 */
+    size: PropTypes.number,
 };
 
-Icon.defaultProps = {
+const defaultProps = {
     type: 'font',
-    size: '',
+    size: 16,
 };
+
+class Icon extends React.Component {
+    render() {
+        const prefix = this.props.type === 'font' ? 'ps-font-icon' : 'ps-image-icon';
+        return (
+            <i
+                className={`${prefix}-${this.props.name}`}
+                style={{
+                    fontSize: this.props.size,
+                }}
+            />
+        );
+    }
+}
+
+Icon.propTypes = propTypes;
+Icon.defaultProps = defaultProps;
 
 export default Icon;
