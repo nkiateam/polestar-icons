ce47ee87a65b8c2a51791ca6591e407867418940
elle510 <elle0510@gmail.com>
2018-06-26 13:59:47 +0900

    Examples.js 링크 추가

--- src/Example/Example.js
+++ src/Example/Example.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FontIcon from './FontIcon';
 import ImageIcon from './ImageIcon';
-import Icon from 'polestar-icons';
+import Icon from '../index';
 
 class Example extends React.Component {
     renderUsage = () => {
@@ -19,7 +19,25 @@ class Example extends React.Component {
         `;
         return (
             <div>
-                <h1>Usage</h1>
+                <h1>
+                    Usage&nbsp;
+                    <a
+                        href="https://github.com/nkiateam/polestar-icons"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <Icon name="github-circled-alt2" />
+                    </a>&nbsp;
+                    <a
+                        href="http://192.168.232.211/polestar-icons/docs"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <Icon name="doc-text" />
+                    </a>
+                </h1>
                 <pre>{usage}</pre>
             </div>
         );
@@ -64,11 +82,11 @@ class Example extends React.Component {
                     />
                 </div>
             </div>
-        )
+        );
     }
 
     renderAnimationIcon = () => {
-        const animations = ['spin', 'spin-pulse', 'wrench', 'ring', 'vertical', 'horizontal', 'flash', 'bounce', 
+        const animations = ['spin', 'spin-pulse', 'wrench', 'ring', 'vertical', 'horizontal', 'flash', 'bounce',
             'float', 'pulse', 'tada', 'passing', 'passing-reverse', 'burst', 'falling'];
         const usage = `
             import Icon from 'polestar-icons';
@@ -84,43 +102,45 @@ class Example extends React.Component {
                 )
             }
         `;
-        const icons = animations.map((animation) => {
-            return (
-                <div key={animation} style={{ minWidth: '250px', margin: '20px 0px' }}>
-                    <Icon
-                        name="spinner"
-                        color="blue"
-                        size={2}
-                        animation={animation}
-                        style={{ margin: '0 10px' }}
-                    />
-                    <Icon
-                        name="spinner1"
-                        color="blue"
-                        size={2}
-                        animation={animation}
-                        style={{ margin: '0 10px' }}
-                    />
-                    <Icon
-                        name="spinner2"
-                        color="blue"
-                        size={2}
-                        animation={animation}
-                        style={{ margin: '0 10px' }}
-                    />
-                    {animation}
-                </div>
-            );
-        });
+        const icons = animations.map((animation) => (
+            <div key={animation} style={{ minWidth: '250px', margin: '20px 0px' }}>
+                <Icon
+                    name="spinner"
+                    color="blue"
+                    size={2}
+                    animation={animation}
+                    style={{ margin: '0 10px' }}
+                />
+                <Icon
+                    name="spinner1"
+                    color="blue"
+                    size={2}
+                    animation={animation}
+                    style={{ margin: '0 10px' }}
+                />
+                <Icon
+                    name="spinner2"
+                    color="blue"
+                    size={2}
+                    animation={animation}
+                    style={{ margin: '0 10px' }}
+                />
+                {animation}
+            </div>
+        ));
         return (
             <div>
                 <h1>Animation Icon</h1>
                 <pre>{usage}</pre>
-                <div style={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap', margin: '10px 30px' }}>
+                <div
+                    style={{
+                        display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap', margin: '10px 30px',
+                    }}
+                >
                     {icons}
                 </div>
             </div>
-        )
+        );
     }
 
     renderFontIcons = () => <FontIcon />;
